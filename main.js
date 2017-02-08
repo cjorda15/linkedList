db13389a76c0ba7e5ca328d719e78e1b96cd2e00
Karen Kepner <kkepner14@gmail.com>
2017-02-08 13:32:02 -0700

    with working read and delete buttons.

--- main.js
+++ main.js
@@ -3,34 +3,43 @@ var $webUrlInput = $('#web-url-input');
 var $enterButton = $('#enter-button');
 var $list = $('#list')
 
+// var $readButton = $('#read-button');
+// var $deleteButton = $('#delete-button');
 
-var $readButton = $('#read-button');
-var $deleteButton = $('#delete-button-button');
-
-var $readButton = $('#read-button');
-var $deleteButton = $('#delete-button-button');
 
  $enterButton.on('click',function(){
-$list.prepend('<article><p id="web-title">'+$webTitleInput.val()+'</p>'+
-'<a id="web-url">'+$webUrlInput.val()+'</a>'+
-'<button id="read-button">Read</button>'+
-'<button id="delete-button">Delete</button>'
-+'</article>')})
+    checkInput();
+    $list.prepend('<article class="card"><p id="web-title">'+$webTitleInput.val()+'</p>'+
+    '<a id="web-url">'+$webUrlInput.val()+'</a>'+
+    '<button id="read-button" class="notread read">Read</button>'+
+    '<button id="delete-button">Delete</button>'
+    +'</article>')
+  })
+
+function checkInput() {
+  if ($webTitleInput === "" || $webUrlInput === ""){
+    alert('ERROR!!!!!!!!!!');
+  }
+}
 
 
+var cards = function(title, url){
+  this.$webTitleInput = title;
+  this.$webUrlInput = url;
+}
 
 //remove a card when delete button is clicked
-$('#delete-button').on('click', function() {
-  this.parentNode.removeChild($('.card'))
-  // $('.card').remove();
-  console.log('pants');
+$('#list').on('click','#delete-button', function() {
+  $(this).parent().remove();
+  // console.log('pants');
 })
 
+
 //change the class of the read button on click
-$('#read-button').on('click', function() {
-  //$('#read-button').css('color','#F05A28');
-  $('#read-button').toggleClass('read notread')
+$('#list').on('click', "#read-button", function() {
+  $(this).toggleClass('notread');
 })
+
 
 
 // $('#read-button-button').on('click'),function(){
