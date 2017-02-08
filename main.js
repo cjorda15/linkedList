b25695a4f4beb89c5553b1463831cd947408ff5e
Karen Kepner <kkepner14@gmail.com>
2017-02-07 22:13:03 -0700

    add font, add delete button

--- main.js
+++ main.js
@@ -7,8 +7,17 @@ var $webUrl =$webUrlInput.val();
 var $readButton = $('#read-button');
 var $deleteButton = $('#delete-button-button');
 
- $enterButton.on('click',function(){
-$list.prepend('<p id="web-title">'+$title+'</p>'
-+'<a id=web-url>'+$webUrl+'</a>'+
-'<button id="read-button">Read</button>'
-+'<button id="delete-button">Delete</button>')})
+ $enterButton.on('click',function() {
+$list.prepend(
+      '<article class="card">' +
+      '<p id="web-title">' + $webTitleInput.val() + '</p>' +
+      '<a id=web-url>' + $webUrlInput.val() + '</a>' +
+      '<button id="read-button" class="read">Read</button>' +
+      '<button id="delete-button" class="delete">Delete</button>' +
+      '</article>')
+    })
+
+//remove a card when delete button is clicked
+$('#delete-button').on('click', function() {
+  $('#list').remove().;
+})
