a15b01ebc24d51bfce29a94786ed5245351c1490
Chris Jordan <cjorda15@msudenver.edu>
2017-02-08 15:39:58 -0700

    dont use

--- main.js
+++ main.js
@@ -3,40 +3,32 @@ var $webUrlInput = $('#web-url-input');
 var $enterButton = $('#enter-button');
 var $list = $('#list')
 
-// var $readButton = $('#read-button');
-// var $deleteButton = $('#delete-button');
 
 
- $enterButton.on('click',function(){
-    checkInput();
+var $readButton = $('#read-button');
+var $deleteButton = $('#delete-button-button');
+var arr = [];
+ $enterButton.on('click',function() {
+$list.prepend(
+      '<article class="card">' +
+      '<p id="web-title">' + $webTitleInput.val() + '</p>' +
+      '<a href= id=web-url>' + $webUrlInput.val() + '</a>' +
+      '<button id="read-button" >Read</button>' +
+      '<button id="delete-button" class="delete">Delete</button>' +
+      '</article>');
+       arr = [this];
     })
 
-
-  function checkInput() {
-    if ($('#web-title-input').val() == "" || $('#web-url-input').val() == "") {
-      alert('Please enter a website title.');
-    } else {
-      $list.prepend('<article class="card"><p id="web-title">'+$webTitleInput.val()+'</p>'+
-      '<a href='+$webUrlInput.val()+' id="web-url" target="_blank">'+$webUrlInput.val()+'</a>'+
-      '<button id="read-button" class="notread read">Read</button>'+
-      '<button id="delete-button">Delete</button>'
-      +'</article>')
-    }
-}
-
-var cards = function(title, url){
-  this.$webTitleInput = title;
-  this.$webUrlInput = url;
-}
+    &&
 
 //remove a card when delete button is clicked
-$('#list').on('click','#delete-button', function() {
-  $(this).parent().remove();
-  // console.log('pants');
+$('#list').on('click','#delete-button',function() {
+
+   $(this).parent().remove()
 })
 
-
 //change the class of the read button on click
-$('#list').on('click', "#read-button", function() {
-  $(this).toggleClass('notread');
+$('#read-button').on('click', ,function() {
+  //$('#read-button').css('color','#F05A28');
+  $('#read-button').toggleClass('read notread')
 })
