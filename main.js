63f9a334d07b0337def84d3b6fa41fc90ee10f71
Chris Jordan <cjorda15@msudenver.edu>
2017-02-08 16:12:56 -0700

    should be the updated version

--- main.js
+++ main.js
@@ -3,32 +3,40 @@ var $webUrlInput = $('#web-url-input');
 var $enterButton = $('#enter-button');
 var $list = $('#list')
 
+// var $readButton = $('#read-button');
+// var $deleteButton = $('#delete-button');
 
 
-var $readButton = $('#read-button');
-var $deleteButton = $('#delete-button-button');
-var arr = [];
- $enterButton.on('click',function() {
-$list.prepend(
-      '<article class="card">' +
-      '<p id="web-title">' + $webTitleInput.val() + '</p>' +
-      '<a href= id=web-url>' + $webUrlInput.val() + '</a>' +
-      '<button id="read-button" >Read</button>' +
-      '<button id="delete-button" class="delete">Delete</button>' +
-      '</article>');
-       arr = [this];
+ $enterButton.on('click',function(){
+    checkInput();
     })
 
-    &&
+
+  function checkInput() {
+    if ($('#web-title-input').val() == "" || $('#web-url-input').val() == "") {
+      alert('Please enter a website title.');
+    } else {
+      $list.prepend('<article class="card"><p id="web-title">'+$webTitleInput.val()+'</p>'+
+      '<a href='+$webUrlInput.val()+' id="web-url" target="_blank">'+$webUrlInput.val()+'</a>'+
+      '<button id="read-button" class="notread read">Read</button>'+
+      '<button id="delete-button">Delete</button>'
+      +'</article>')
+    }
+}
+
+var cards = function(title, url){
+  this.$webTitleInput = title;
+  this.$webUrlInput = url;
+}
 
 //remove a card when delete button is clicked
-$('#list').on('click','#delete-button',function() {
-
-   $(this).parent().remove()
+$('#list').on('click','#delete-button', function() {
+  $(this).parent().remove();
+  // console.log('pants');
 })
 
+
 //change the class of the read button on click
-$('#read-button').on('click', ,function() {
-  //$('#read-button').css('color','#F05A28');
-  $('#read-button').toggleClass('read notread')
+$('#list').on('click', "#read-button", function() {
+  $(this).toggleClass('notread');
 })
