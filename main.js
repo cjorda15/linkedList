22e8dd6213aa6286bb3dfd73b5453d0b6ce6e9b7
Karen Kepner <kkepner14@gmail.com>
2017-02-08 08:58:20 -0700

    working in read and delete buttons, add readme.md

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ var $webTitleInput = $('#web-title-input');
 var $webUrlInput = $('#web-url-input');
 var $enterButton = $('#enter-button');
 var $list = $('#list')
-var $title = $webTitleInput.val();
-var $webUrl =$webUrlInput.val();
+
+
 var $readButton = $('#read-button');
 var $deleteButton = $('#delete-button-button');
 
@@ -12,12 +12,20 @@ $list.prepend(
       '<article class="card">' +
       '<p id="web-title">' + $webTitleInput.val() + '</p>' +
       '<a id=web-url>' + $webUrlInput.val() + '</a>' +
-      '<button id="read-button" class="read">Read</button>' +
+      '<button id="read-button" >Read</button>' +
       '<button id="delete-button" class="delete">Delete</button>' +
       '</article>')
     })
 
 //remove a card when delete button is clicked
 $('#delete-button').on('click', function() {
-  $('#list').remove().;
+  this.parentNode.removeChild($('.card'))
+  // $('.card').remove();
+  console.log('pants');
+})
+
+//change the class of the read button on click
+$('#read-button').on('click', function() {
+  //$('#read-button').css('color','#F05A28');
+  $('#read-button').toggleClass('read notread')
 })
